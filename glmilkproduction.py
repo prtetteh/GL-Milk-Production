6d36be066cc8674dc2a9930e3a434ca24d8ac275
prtetteh <fiifilotetteh@gmail.com>
2024-11-14 14:00:27 -0600

    Update glmilkproduction.py

--- glmilkproduction.py
+++ glmilkproduction.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scikit_learn.model_selection import train_test_split
+#from scikit_learn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+
 
 # Set up Streamlit layout and theme
 st.set_page_config(layout="wide", page_title="Milk Production Analysis")
